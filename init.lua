301e69bd3ca7ac6490274b6b869a2e723131c0a0
csorndawg <zaccsorny@yahoo.com>
2024-10-26 00:39:18 +0000

    Add autocmds file

--- init.lua
+++ init.lua
@@ -93,11 +93,14 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
--- [[ Setting options ]]
+-- [[ NVIM Options ]]
 require 'options'
 
--- [[ Basic Keymaps ]]
+-- [[ NVIM Basic Keymaps ]]
 require 'keymaps'
+
+-- [[ NVIM Autocmds ]]
+require 'autocmds'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
@@ -105,9 +108,15 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- @TODO
+-- [[ NVIM Plugin Keymaps/Rebinds ]]
+--require 'plug-keymaps'
+
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
 
+-- @TODO - add LSP, CMP, and any other post plugin configuration file
 -- SOURCE POST-INSTALL BUNDLED PLUGIN LOGIC (eg. colorscheme.lua)
 require 'after.colorscheme'
